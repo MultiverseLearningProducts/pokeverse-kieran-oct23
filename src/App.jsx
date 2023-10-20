89d63914dd4df8ec75b4638b070a1619d111d79e
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2023-10-20 10:54:00 +0100

    Add search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
 import Navbar from "react-bootstrap/Navbar";
 import Row from "react-bootstrap/Row";
 import PokemonCard from "./components/PokemonCard";
@@ -12,11 +14,17 @@ const POKE_API = `https://pokeapi.co/api/v2/pokemon?limit=${LIMIT}`;
 
 function App() {
 	const { data, error, isLoading } = useSWR(POKE_API, fetcher);
+	const [searchTerms, setSearchTerms] = useState("");
 
 	// TODO: Handle error and loading states
 	if (error || isLoading) return;
 
-	const chunks = chunkArray(data.results, 6);
+	function includesSearchTerms({ name }) {
+		return name.toLowerCase().includes(searchTerms.toLowerCase());
+	}
+
+	const filtered = data.results.filter(includesSearchTerms);
+	const chunks = chunkArray(filtered, 6);
 
 	return (
 		<>
@@ -35,16 +43,28 @@ function App() {
 				</Container>
 			</Navbar>
 
-			<Container>
-				{chunks.map((chunk, index) => (
-					<Row key={index} className="my-4">
-						{chunk.map(pokemon => (
-							<Col key={pokemon.name}>
-								<PokemonCard url={pokemon.url} />
-							</Col>
-						))}
-					</Row>
-				))}
+			<Container className="mt-4">
+				<Form role="search" className="col-2 mx-auto">
+					<Form.Control
+						type="search"
+						aria-label="Search"
+						placeholder="Search..."
+						value={searchTerms}
+						onChange={e => void setSearchTerms(e.target.value)}
+					/>
+				</Form>
+
+				<div>
+					{chunks.map((chunk, index) => (
+						<Row key={index} className="my-4">
+							{chunk.map(pokemon => (
+								<Col key={pokemon.name}>
+									<PokemonCard url={pokemon.url} />
+								</Col>
+							))}
+						</Row>
+					))}
+				</div>
 			</Container>
 		</>
 	);
